448145f3cf04dee06680304489a8bdcd71a533f7
Sequentz <wouterkvandenbossche@gmail.com>
2024-11-15 14:01:55 +0100

    api routes

---
+++ app/Http/Controllers/WordApiController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\WordResource;
+use App\Models\Word;
+use Illuminate\Http\Request;
+
+class WordApiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return WordResource::collection(Word::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Word $word)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Word $word)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Word $word)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +17,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('v1')->group(function () {
+    Route::apiResource('words', \App\Http\Controllers\WordApiController::class);
+    Route::apiResource('themes', \App\Http\Controllers\ThemeApiController::class);
 });
