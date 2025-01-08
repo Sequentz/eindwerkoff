51c28b3acce43a86fcfa50c4acf558792ba86bdd
Sequentz <wouterkvandenbossche@gmail.com>
2025-01-08 18:30:58 +0100

    https issue

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,7 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (config('APP.ENV') == ! 'local') {
+        if (env('APP_ENV') === 'production') {
             URL::forceScheme('https');
         }
     }
