22c2eac592735c295dc5af4f74dbb2fdea6c8853
Sequentz <wouterkvandenbossche@gmail.com>
2025-01-08 17:57:10 +0100

    fixed

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,7 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (config('app.env') === 'production') {
+        if (config('APP.ENV') == ! 'local') {
             URL::forceScheme('https');
         }
     }
