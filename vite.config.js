7c1cc354533828b093d687bfa6fccbe9d182f53b
Sequentz <wouterkvandenbossche@gmail.com>
2025-01-08 18:04:20 +0100

    https issue

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,9 @@ import laravel from "laravel-vite-plugin";
 export default defineConfig({
     server: {
         https: true,
+        host: true, // Allow external access if needed
     },
+    base: "/", // Set base path for assets
     plugins: [
         laravel({
             input: ["resources/css/app.css", "resources/js/app.js"],
